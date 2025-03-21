607c624ba41f57c8c4711b469202791f23bbbbbe
Ribhi El-Zaru <ribhielzaru@gmail.com>
2025-03-22 01:00:48 +0200

    add middleware

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
+import compression from 'compression';
 import express from 'express';
 import interestRoutes from './routes/InterestRoutes';
 import languageRoutes from './routes/LanguagesRoutes';
 import userRoutes from './routes/UserRoutes';
 
+import bodyParser from 'body-parser';
+import morganBody from 'morgan-body';
+
 const app = express();
 
-// Middleware to parse JSON bodies
-app.use(express.json());
+// must parse body before morganBody as body will be logged
+app.use(bodyParser.json());
+morganBody( // hook up morgan to express app.
+    app,
+    { filterParameters: ["email", "phoneNumber", "dateOfBirth", "location"] }); // Example filters. (wish I could add user name but doesn't work as it filters others.)
+app.use(compression())
 
 // Routes
 app.use('/api/users', userRoutes);
